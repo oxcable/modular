a157ab2c43100b91662dcad9297e95d9cddb81dd
Michael Nye <thenyeguy@gmail.com>
2022-04-01 12:43:11 -0700

    Move parameter utils into submodule.

--- module/src/lib.rs
+++ module/src/lib.rs
@@ -5,12 +5,14 @@
 use std::{
     collections::HashMap,
     hash::{Hash, Hasher},
-    sync::atomic::{AtomicU8, Ordering},
 };
 
-use eurorack::{utils::Duration, Voltage};
+use eurorack::Voltage;
 
+pub mod parameters;
 pub mod registry;
+
+pub use parameters::{Parameter, SerializedParameter};
 
 pub trait AudioUnit: Send {
     fn reset(&mut self, sample_rate: usize);
@@ -84,77 +86,5 @@ impl Hash for ModuleOutput {
         std::any::TypeId::of::<Self>().hash(state);
         self.module.hash(state);
         self.channel.hash(state);
-    }
-}
-
-#[derive(Debug, serde::Serialize, serde::Deserialize)]
-#[serde(untagged)]
-pub enum SerializedParameter {
-    Num(f32),
-    List(Vec<Box<SerializedParameter>>),
-}
-
-impl SerializedParameter {
-    fn as_num(&self) -> f32 {
-        match self {
-            SerializedParameter::Num(value) => *value,
-            _ => panic!("SerializedParameter is not f32"),
-        }
-    }
-}
-
-pub trait Parameter {
-    type Value;
-    fn read(&self) -> Self::Value;
-    fn write(&self, value: Self::Value);
-    fn serialize(&self) -> SerializedParameter;
-    fn deserialize(&self, serialized: &SerializedParameter);
-}
-
-impl Parameter for AtomicU8 {
-    type Value = u8;
-    fn read(&self) -> Self::Value {
-        self.load(Ordering::Relaxed)
-    }
-    fn write(&self, value: Self::Value) {
-        self.store(value, Ordering::Relaxed)
-    }
-    fn serialize(&self) -> SerializedParameter {
-        SerializedParameter::Num(self.read() as f32)
-    }
-    fn deserialize(&self, serialized: &SerializedParameter) {
-        self.write(serialized.as_num() as u8);
-    }
-}
-
-impl Parameter for portable_atomic::AtomicF32 {
-    type Value = f32;
-    fn read(&self) -> Self::Value {
-        self.load(Ordering::Relaxed)
-    }
-    fn write(&self, value: Self::Value) {
-        self.store(value, Ordering::Relaxed)
-    }
-    fn serialize(&self) -> SerializedParameter {
-        SerializedParameter::Num(self.read())
-    }
-    fn deserialize(&self, serialized: &SerializedParameter) {
-        self.write(serialized.as_num());
-    }
-}
-
-impl Parameter for Duration {
-    type Value = f32;
-    fn read(&self) -> Self::Value {
-        self.seconds()
-    }
-    fn write(&self, value: Self::Value) {
-        self.set_seconds(value);
-    }
-    fn serialize(&self) -> SerializedParameter {
-        SerializedParameter::Num(self.read())
-    }
-    fn deserialize(&self, serialized: &SerializedParameter) {
-        self.write(serialized.as_num());
     }
 }
---
+++ module/src/parameters.rs
@@ -0,0 +1,75 @@
+use std::sync::atomic::{AtomicU8, Ordering};
+
+use eurorack::utils::Duration;
+
+#[derive(Debug, serde::Serialize, serde::Deserialize)]
+#[serde(untagged)]
+pub enum SerializedParameter {
+    Num(f32),
+    List(Vec<Box<SerializedParameter>>),
+}
+
+impl SerializedParameter {
+    fn as_num(&self) -> f32 {
+        match self {
+            SerializedParameter::Num(value) => *value,
+            _ => panic!("SerializedParameter is not f32"),
+        }
+    }
+}
+
+pub trait Parameter {
+    type Value;
+    fn read(&self) -> Self::Value;
+    fn write(&self, value: Self::Value);
+    fn serialize(&self) -> SerializedParameter;
+    fn deserialize(&self, serialized: &SerializedParameter);
+}
+
+impl Parameter for AtomicU8 {
+    type Value = u8;
+    fn read(&self) -> Self::Value {
+        self.load(Ordering::Relaxed)
+    }
+    fn write(&self, value: Self::Value) {
+        self.store(value, Ordering::Relaxed)
+    }
+    fn serialize(&self) -> SerializedParameter {
+        SerializedParameter::Num(self.read() as f32)
+    }
+    fn deserialize(&self, serialized: &SerializedParameter) {
+        self.write(serialized.as_num() as u8);
+    }
+}
+
+impl Parameter for portable_atomic::AtomicF32 {
+    type Value = f32;
+    fn read(&self) -> Self::Value {
+        self.load(Ordering::Relaxed)
+    }
+    fn write(&self, value: Self::Value) {
+        self.store(value, Ordering::Relaxed)
+    }
+    fn serialize(&self) -> SerializedParameter {
+        SerializedParameter::Num(self.read())
+    }
+    fn deserialize(&self, serialized: &SerializedParameter) {
+        self.write(serialized.as_num());
+    }
+}
+
+impl Parameter for Duration {
+    type Value = f32;
+    fn read(&self) -> Self::Value {
+        self.seconds()
+    }
+    fn write(&self, value: Self::Value) {
+        self.set_seconds(value);
+    }
+    fn serialize(&self) -> SerializedParameter {
+        SerializedParameter::Num(self.read())
+    }
+    fn deserialize(&self, serialized: &SerializedParameter) {
+        self.write(serialized.as_num());
+    }
+}
